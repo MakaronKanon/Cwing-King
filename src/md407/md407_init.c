9eb777c7a539fa978e0d93615226c6e5c21bc986
Marcus Jörgensson <marcusjorgensson@gmail.com>
2019-02-28 18:47:30 +0100

    removed platform dependent code from startup.c

--- src/md407/md407_init.c
+++ src/md407/md407_init.c
@@ -1,5 +1,18 @@
 #include "md407_ports.h"
 
+
+void startup(void) __attribute__((naked)) __attribute__((section (".start_section")) );
+
+void startup ( void )
+{
+__asm volatile(
+	" LDR R0,=0x2001C000\n"		/* set stack */
+	" MOV SP,R0\n"
+	" BL main\n"				/* call main */
+	"_exit: B .\n"				/* never return */
+	) ;
+}
+
 #define GPIO_D_BASE (0x40020C00)
 typedef volatile unsigned int portType;
 #define portD ((portType*) GPIO_D_BASE)
--- src/startup.c
+++ src/startup.c
@@ -1,43 +1,3 @@
-/*
- * 	startup.c
- *
- */
- 
- #ifndef WINDOWS
-void startup(void) __attribute__((naked)) __attribute__((section (".start_section")) );
-
-void startup ( void )
-{
-__asm volatile(
-	" LDR R0,=0x2001C000\n"		/* set stack */
-	" MOV SP,R0\n"
-	" BL main\n"				/* call main */
-	"_exit: B .\n"				/* never return */
-	) ;
-}
-
-#endif
-
-#ifdef WINDOWS
-#include "stdio.h"
-#endif
-
-
-
-#ifdef MD407
-
-/*
-void init_app()
-{
-	// 7seg-disp port D0-7
-		
-	// Ställ in GPIO_MODER
-	*portD &= 0xFFFF0000;
-	*portD |= 0x00005555;
-}*/
-
-#endif
-
 #include "graphics.h"
 #include "delay.h"
 #include "keycodes.h"
@@ -68,11 +28,13 @@ void main(void)
 			playing = 0;
 		}
 		*/
+		
+		
 		update();
-		clearBuffer();
 		render();
 		displayBuffer();
-		
+		clearBuffer();
+
 		// 30fps -> 1/30 = 33ms each frame
 		delayMilliSecs(33); // Somewhat target 30 fps.
 	}
